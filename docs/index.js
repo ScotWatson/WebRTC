89d9cf137efc647f106c814338f73c3b21912dfd
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-10-28 21:25:05 -0400

    Create index.js

---
+++ docs/index.js
@@ -0,0 +1,49 @@
+/*
+(c) 2022 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+"use strict";
+
+const initPageTime = performance.now();
+
+const loadWindow = new Promise(function (resolve, reject) {
+  window.addEventListener("load", function (evt) {
+    resolve(evt);
+  });
+});
+
+const loadErrorLogModule = (async function () {
+  try {
+    const module = await import("https://scotwatson.github.io/Debug/ErrorLog.mjs");
+    return module;
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+(async function () {
+  try {
+    const modules = await Promise.all( [ loadWindow, loadErrorLogModule ] );
+    start(modules);
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+async function start( [ evtWindow, ErrorLog ] ) {
+  try {
+    (async function () {
+      const rtc1 = new RTCPeerConnection();
+      const offer = await rtc1.createOffer();
+      const result = document.createElement("p");
+      result.appendChild(document.createTextNode(offer));
+      document.body.appendChild(result);
+    })();
+  } catch (e) {
+    ErrorLog.rethrow({
+      functionName: "start",
+      error: e,
+    });
+  }
+}
